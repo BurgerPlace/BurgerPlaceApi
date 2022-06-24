e2a96c0def9546360240135d8e2a2bc0bad3dd39
Michal Witek <michalwitek1232@gmail.com>
2022-06-24 17:12:01 +0200

    kateogorie

--- BurgerPlace/GlobalUsings.cs
+++ BurgerPlace/GlobalUsings.cs
@@ -1,7 +1,8 @@
 ﻿global using System.ComponentModel.DataAnnotations;
 global using BurgerPlace.Roles;
+global using BurgerPlace.Context;
+global using BurgerPlace.Models.Database;
+global using Microsoft.EntityFrameworkCore;
+global using System.Net;
 
-namespace GlobalUsings
-{
-    
-}
+namespace GlobalUsings { }
